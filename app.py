f1956c5929eba38026c7f7a39986c1903c202e04
Ajuogaaz <brianlinus1753@gmail.com>
2019-08-21 18:04:11 +0300

    Added the front end data for the system and connected to route

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 """This a simulation of a web page server backend for displaying a management system backend data"""
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route('/departments')
+def departments():
+    return render_template("departments.html")
+
+@app.route('/employees')
+def employees():
+    return render_template("employees.html")
 
 
 if __name__ == '__main__':
